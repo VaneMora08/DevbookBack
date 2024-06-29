1adc5a0b607d77194079e80dff065373a289b300
Yuli Leon <yuli.leon@genstudents.org>
2024-06-29 15:52:26 -0500

    Creación de las entidades

--- src/main/java/com/devbook/formattech/entity/Comment.java
+++ src/main/java/com/devbook/formattech/entity/Comment.java
@@ -1,4 +1,44 @@
 package com.devbook.formattech.entity;
 
+// que tiene que llevar aquí
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.w3c.dom.Text;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comment")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String comm_content;
+    private String comm_media_url;
+    private String comm_picture_url;
+
+//    @Timestamp
+//    private Date created_at;
+//    @Timestamp
+//    private Date updated_at;
+//    @ManyToOne
+//    @JoinColumn(name = "post_id", nullable = false) o table
+//    private Post post;
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
 }
--- src/main/java/com/devbook/formattech/entity/Rol.java
+++ src/main/java/com/devbook/formattech/entity/Rol.java
@@ -1,10 +1,17 @@
 package com.devbook.formattech.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "rol")
 public class Rol {
@@ -18,4 +25,5 @@ public class Rol {
 //    @ManyToMany
 //    @JoinTable(name = "user_id")
 //    private Set<User> user = new HashSet<>();
+    // hay una foranea de stack
 }
