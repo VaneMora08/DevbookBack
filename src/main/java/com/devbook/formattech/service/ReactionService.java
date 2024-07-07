f9c3df293a9c707be2df76147e06fe46ca163773
Yuli Leon <yuli.leon@genstudents.org>
2024-07-07 17:10:14 -0500

    Creacion CRUD de Reaction

--- src/main/java/com/devbook/formattech/service/ReactionService.java
+++ src/main/java/com/devbook/formattech/service/ReactionService.java
@@ -1,6 +1,5 @@
 package com.devbook.formattech.service;
 
-import com.devbook.formattech.Dto.CountryDto;
 import com.devbook.formattech.Dto.ReactionDto;
 
 import java.util.List;
@@ -10,6 +9,6 @@ public interface ReactionService {
     ReactionDto createReaction (ReactionDto reactionDto);
     List<ReactionDto> getAllReactions();
     ReactionDto getReactionById(int id);
-    ReactionDto updateReaction(int id, CountryDto countryDto);
-    void deleteCountry(int id);
+    ReactionDto updateReaction(int id, ReactionDto reactionDto);
+    void deleteReaction(int id);
 }
