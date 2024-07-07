434ba746b1d9ab1ae4086245316a5f27178da020
Yuli Leon <yuli.leon@genstudents.org>
2024-07-07 16:18:51 -0500

    En proceso

--- src/main/java/com/devbook/formattech/service/ReactionService.java
+++ src/main/java/com/devbook/formattech/service/ReactionService.java
@@ -1,4 +1,15 @@
 package com.devbook.formattech.service;
 
+import com.devbook.formattech.Dto.CountryDto;
+import com.devbook.formattech.Dto.ReactionDto;
+
+import java.util.List;
+
 public interface ReactionService {
+
+    ReactionDto createReaction (ReactionDto reactionDto);
+    List<ReactionDto> getAllReactions();
+    ReactionDto getReactionById(int id);
+    ReactionDto updateReaction(int id, CountryDto countryDto);
+    void deleteCountry(int id);
 }
