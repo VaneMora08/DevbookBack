a0701f36b281373774cc4436449f1ee7af9bffe3
Yuli Leon <yuli.leon@genstudents.org>
2024-07-07 13:45:48 -0500

    Creacion CRUD de Gender

--- src/main/java/com/devbook/formattech/service/implementation/GenderServiceImpl.java
+++ src/main/java/com/devbook/formattech/service/implementation/GenderServiceImpl.java
@@ -1,6 +1,66 @@
 package com.devbook.formattech.service.implementation;
 
+import com.devbook.formattech.Dto.GenderDto;
+import com.devbook.formattech.converter.GenderMapper;
+import com.devbook.formattech.entity.Gender;
+import com.devbook.formattech.exceptions.ResourceNotFoundException;
+import com.devbook.formattech.repository.GenderRepository;
 import com.devbook.formattech.service.GenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GenderServiceImpl implements GenderService {
+
+    private static final Logger log = LoggerFactory.getLogger(GenderServiceImpl.class);
+
+    @Autowired
+    private GenderRepository genderRepository;
+
+    @Autowired
+    private GenderMapper genderMapper;
+
+
+    @Override
+    public GenderDto createGender(GenderDto genderDto) {
+        Gender gender = genderMapper.gender(genderDto);
+        gender = genderRepository.save(gender);
+        return genderMapper.genderDto (gender);
+    }
+
+
+
+    @Override
+    public List<GenderDto> getAllGenders() {
+        return genderRepository.findAll().stream()
+                .map(GenderMapper::genderDto).collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public GenderDto getGenderById(int id) {
+        return genderRepository.findById(id)
+                .map(GenderMapper::genderDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Gender not found with id: " + id));
+    }
+
+
+    @Override
+    public GenderDto updateGender(int id, GenderDto genderDto) {
+        Gender existingGender = genderRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Gender not found with id: " + id));
+        existingGender.setName(genderDto.getName());
+        Gender updatedGender = genderRepository.save(existingGender);
+        return genderMapper.genderDto(updatedGender);
+    }
+
+    @Override
+    public void deleteGender(int id) {
+        Gender existingGender = genderRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Gender not found with id: " + id));
+        genderRepository.delete(existingGender);
+    }
+
 }
